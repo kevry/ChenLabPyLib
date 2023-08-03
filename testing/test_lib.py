844bbdee5365ff31653842290f36d7f1909240fb
Kevin Delgado <thekevry@gmail.com>
2023-08-03 12:58:26 -0400

    Create test_lib.py

---
+++ testing/test_lib.py
@@ -0,0 +1,24 @@
+import ChenLabPyLib
+import sys
+
+def test_chenlabpaths1():
+  # testing chenlab_filepaths function still follows functionality
+  test_path = "/net/claustrum/mnt/data/Projects/Homecage/TestFile.txt"
+  converted_path = ChenLabPyLib.chenlab_filepaths(test_path)
+  
+  if sys.platform == 'linux': # on linux/SCC
+    assert converted_path == test_path
+  else: # on Windows
+    assert converted_path == 'Z:\\Projects\\Homecage\\TestFile.txt'
+
+def test_chenlabpaths2():
+  # testing chenlab_filepaths function still follows functionality
+  test_path = "/net/claustrum4/mnt/storage/data/Projects/ARG"
+  converted_path = ChenLabPyLib.chenlab_filepaths(test_path)
+  
+  if sys.platform == 'linux': # on linux/SCC
+    assert converted_path == test_path
+  else: # on Windows
+    assert converted_path == 'V:\\Projects\\ARG'
+    
+  
